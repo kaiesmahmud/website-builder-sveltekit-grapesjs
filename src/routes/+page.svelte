4f906a269a19652dbffd6cf883adf9701ad8913e
kaiesmahmud <kaiesmahmud@gmail.com>
2023-11-26 22:33:53 +0600

    updated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import NewWebsiteNameComponent from '../lib/components/process-1/NewWebsiteNameComponent.svelte';
+    import { processStep } from '../lib/store/store';
 
-<h1 class="text-3xl font-bold underline text-center">
-    Hello world!
-  </h1>
+  
+</script>
+
+
+
+<h1 class="py-5 text-3xl md:text-5xl font-bold  text-center text-transparent capitalize bg-clip-text bg-gradient-to-r from-cyan-400 to-pink-600">
+  Custom-Website-Builder
+</h1>
+{#if $processStep == 1}
+   <!-- First Step Component here -->
+   <div class="flex gap-3 items-center justify-center w-full">
+    <NewWebsiteNameComponent/>
+  </div>
+{:else if  $processStep == 2}
+   <!-- Second Step Component here -->
+   <h1>Step 2 Now</h1>
+{:else if  $processStep == 3}
+   <!-- Third Step Component here -->
+{:else if  $processStep == 4}
+   <!-- Fourth Step Component here -->
+{:else if  $processStep == 5}
+   <!-- else if content here -->
+{:else}
+   <!-- else content here -->
+{/if}
