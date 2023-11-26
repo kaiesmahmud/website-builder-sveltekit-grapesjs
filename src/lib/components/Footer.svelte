c9e730b64bea6b1b8bd987257ab916d38376918a
kaiesmahmud <kaiesmahmud@gmail.com>
2023-11-26 22:45:38 +0600

    footer added

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,4 @@
+<footer class="text-sm font-bold text-center text-transparent capitalize bg-clip-text bg-gradient-to-r from-cyan-400 to-pink-600">
+    Developed By Kaies Mahmud Nehal <br>
+    2023 | November
+</footer>
