e2439ff9dd9162f6ac8246f4558a296a81d34784
kaiesmahmud <kaiesmahmud@gmail.com>
2023-12-03 22:08:44 +0600

    navbar added

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,3 @@
+<h1 class="py-5 p-1 w-full text-3xl md:text-5xl font-bold  text-center text-transparent capitalize bg-clip-text bg-gradient-to-r from-sky-500 via-cyan-400 to-sky-600">
+    Custom-Website-Builder
+</h1>
