75f276c928b5aeb877c3382e1bee025f7ae4e0b6
kaiesmahmud <kaiesmahmud@gmail.com>
2023-12-03 22:08:52 +0600

    sidebar updated

---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,83 @@
+<script>
+    import Icon from '@iconify/svelte';
+    import { ShowProcess } from '../store/store';
+    let sideBarData = [
+        {title:"Website Name & Logo", value:"step-1" ,icon:"icon-park-outline:edit-name"},
+        {title:"Website Menulists", value:"step-2" ,icon:"material-symbols-light:dashboard-customize-outline" },
+        {title:"Page List", value:"step-3" ,icon:"iconoir:page" },
+        {title:"Page Customization", value:"step-4" ,icon:"gridicons:customize"  },
+        {title:"Add Meta Data", value:"step-5" ,icon:"carbon:flow-data" },
+    ]
+    // for collapsing sidbar -----------
+    let minify =false ; 
+    const handleMinify = ()=>{
+        minify =!minify
+    }
+    let short = false
+    const handleshortingSideBar = ()=>{short=!short}
+    // ============Change Process View ==============
+    const handleProcessChange = (newProcessToView)=>{
+        ShowProcess.set(newProcessToView)
+    }
+
+</script>
+
+<div class={`w-full  text-white border-white rounded p-3 ${minify && " w-auto "}  ${!minify && " md:w-1/4 "} `}>
+
+    <div class="flex items-center justify-between">
+        {#if !minify}
+        <h1 class="text-xl md:text-2xl font-bold text-transparent capitalize bg-clip-text bg-gradient-to-r from-cyan-400 to-sky-600">Magic Creator</h1>
+        {/if}
+        <button on:click={handleMinify} class=" hidden md:block p-3 text-xl font-bold rounded-full bg-white/10 hover:bg-white/20 hover:text-cyan-500">
+            <Icon icon="gridicons:menus" />
+        </button>
+        <button on:click={handleshortingSideBar} class=" md:hidden  p-3 text-xl font-bold rounded-full bg-white/10 hover:bg-white/20 hover:text-cyan-500">
+            <Icon icon="gridicons:menus" />
+        </button>
+    </div>
+    <div class="hidden md:flex flex-col gap-3 text-start items-start justify-normal w-full my-5">
+        {#each sideBarData as {title,value,icon}}
+        <button 
+        on:click={()=> handleProcessChange(value)} 
+        class={`w-full  text-start bg-white/10 hover:bg-white/20 hover:text-cyan-500 rounded-lg flex gap-2 justify-between p-3 text-xs md:text-sm font-semibold ${$ShowProcess === value && " bg-white/20 text-cyan-500 "}`}
+        
+        >
+            <div class="flex gap-2">
+                <div class="text-xl">
+                    <Icon icon={icon} />
+                </div>
+                {#if !minify}
+                     <p>{title}</p>
+                {/if}
+            </div>
+            {#if !minify}
+            <div class="text-xl">
+                <Icon icon="solar:arrow-right-line-duotone" />
+            </div>
+            {/if}
+        </button>
+        {/each}
+    </div>
+    {#if short}
+    <div class="flex md:hidden flex-col gap-3 text-start items-start justify-normal w-full my-5">
+        {#each sideBarData as {title,value,icon}}
+        <button class="w-full  text-start bg-white/10 hover:bg-white/20 hover:text-cyan-500 rounded-lg flex gap-2 justify-between p-3 text-xs md:text-sm font-semibold">
+            <div class="flex gap-2">
+                <div class="text-xl">
+                    <Icon icon={icon} />
+                </div>
+                {#if !minify}
+                     <p>{title}</p>
+                {/if}
+            </div>
+            {#if !minify}
+            <div class="text-xl">
+                <Icon icon="solar:arrow-right-line-duotone" />
+            </div>
+            {/if}
+        </button>
+        {/each}
+
+    </div>
+    {/if}
+</div>
