4538618e53b8dee347bf593e05fe0db310291956
kaiesmahmud <kaiesmahmud@gmail.com>
2023-12-03 22:13:00 +0600

    layout updated

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,12 @@
 <script>
+	import Navbar from './../lib/components/Navbar.svelte';
     import "../app.css";
     import Footer from '../lib/components/Footer.svelte';
   </script>
-  <div class="min-h-screen p-3">
-    <slot />
-    <Footer/>
+  <div class="flex items-center justify-center flex-col gap-10 ">
+    <div class="min-h-screen p-3 max-w-[3240px] w-full ">
+      <Navbar/>
+      <slot />
+      <Footer/>
+    </div>
   </div>
