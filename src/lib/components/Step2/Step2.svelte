36feacf92a04bc3bc09cbda1efd724dc53a57fe2
kaiesmahmud <kaiesmahmud@gmail.com>
2023-12-06 21:41:14 +0600

    working on step-2

--- src/lib/components/Step2/Step2.svelte
+++ src/lib/components/Step2/Step2.svelte
@@ -1,7 +1,11 @@
 <script>
+    import { UserAllData } from "../../store/store";
+    import Dnd from "./sub-components/DND.svelte";
+
 
 </script>
 
 <section>
     Step -2
+    <!-- <Dnd/> -->
 </section>
