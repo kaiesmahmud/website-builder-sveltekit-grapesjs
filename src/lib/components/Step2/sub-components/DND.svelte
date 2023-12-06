74b85c3db7fde6b96c4d1fac89ed60eb87d0a101
kaiesmahmud <kaiesmahmud@gmail.com>
2023-12-06 22:13:20 +0600

    working on dnd

--- src/lib/components/Step2/sub-components/DND.svelte
+++ src/lib/components/Step2/sub-components/DND.svelte
@@ -1,41 +1,31 @@
 <script>
 	import DNDNavbar from './DNDNavbar.svelte';
-    import ParentDnd from './ParentDND.svelte';
-	// import ParentDND from '.ParentDND.svelte';
-	ParentDnd
-	// import { writable } from 'svelte/store';
-	// import { MenuListResult, UserAllData } from '../../../store/store.js';
+    import ParentDND from './ParentDND.svelte';
+	import { writable } from 'svelte/store';
+	import { MenuListResult, UserAllData } from '../../../store/store.js';
 
-// let editNavbar = writable(false) ;
-// const handleEditNavbar = ()=> {
-//     console.log("handleEditNavbar Clicked result - ", $editNavbar)
-//     editNavbar.set(!$editNavbar)
-// }
-// MenuListResult
+let editNavbar = writable(false) ;
+const handleEditNavbar = ()=> {
+    console.log("handleEditNavbar Clicked result - ", $editNavbar)
+    editNavbar.set(!$editNavbar)
+}
 </script>
 
 
-<div class="w-[100%] min-h-[80vh]">
-	<div class=" flex flex-col items-center  justify-center gap-10 py-10 px-3">
-		<!-- {#if $UserAllData.logo.length > 5}
-		<div class="w-40 md:w-32 rounded">
-			<img src={$UserAllData.logo} alt="logo">
-		</div>
-		{/if} -->
-		<!-- <h3 class="p-2 font-bold text-xl lg:text-4xl  text-transparent capitalize bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-			{$UserAllData.websiteName}
-		</h3>
+<div class="w-[100%] min-h-[70vh] max-w-[1500px]">
+	<div class="w-[100%] flex flex-col items-center  justify-center gap-10 py-10 px-3">
 		{#if !$editNavbar}
-		<div class="w-[100%] flex items-center justify-center gap-5">
-			<button class="bg-green-500/20 p-5 rounded  " on:click={handleEditNavbar}>Edit Navbar</button>
-			<button class="bg-green-500/50 p-5 rounded  " on:click={handleEditNavbar}>Next Step</button>
-		</div>
 		<div class="w-[100%]">
 			<DNDNavbar nodes={$MenuListResult}/>
 		</div>
+		<div class="w-[100%] flex items-center justify-center gap-5 font-semibold">
+			<button class="bg-green-500/50 hover:bg-green-600 p-5 rounded  " on:click={handleEditNavbar}>Edit Navbar</button>
+			<button class="bg-green-500/50 hover:bg-green-600 p-5 rounded  " on:click={handleEditNavbar}>Next Step</button>
+		</div>
 		{/if}
 		{#if $editNavbar}
+
 			<ParentDND handleEditNavbar={handleEditNavbar} {MenuListResult} nodes={$MenuListResult} />
-		{/if} -->
+		{/if}
 	</div>
 </div>
