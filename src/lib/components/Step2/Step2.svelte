7219a845518336d6a4cb94d2929fc0a27f50f466
kaiesmahmud <kaiesmahmud@gmail.com>
2023-12-06 23:27:22 +0600

    title  added

--- src/lib/components/Step2/Step2.svelte
+++ src/lib/components/Step2/Step2.svelte
@@ -1,10 +1,11 @@
 <script>
-    import { UserAllData } from "../../store/store";
+    import { ShowProcess, UserAllData } from "../../store/store";
     import Dnd from "./sub-components/DND.svelte";
 
 
 </script>
 
-<section class="w-full p-1">
+<section class="w-full ">
+    <h1 class=" capitalize font-semibold text-xl text-white/80 p-3">{$ShowProcess} of 5 | Edit Navbar Menulist</h1>
     <Dnd/>
 </section>
