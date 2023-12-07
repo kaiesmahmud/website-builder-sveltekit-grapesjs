af29c03ae7036c705f1b3a4f3b433f61c3535cbe
kaiesmahmud <kaiesmahmud@gmail.com>
2023-12-07 21:52:24 +0600

    pageslist for edit added

---
+++ src/lib/components/Step5/sub-components/PagesList.svelte
@@ -0,0 +1,58 @@
+<script>
+    import { UserAllData } from "../../../store/store";
+
+
+
+</script>
+
+<div class="flex flex-col gap-3 p-3 max-w-[700px] w-full ">
+    <!-- ----------Home page ------------ -->
+    <div class="rounded p-2 bg-white/10 font-semibold text-md flex items-center justify-between px-5">
+        <p class=" capitalize">
+            {$UserAllData?.websiteName}
+        </p>
+        <a href={`/${$UserAllData?.websiteName}`} class=" text-white/70 hover:text-white/90">
+            /{$UserAllData?.websiteName}
+        </a>
+        <button class="rounded bg-sky-500 hover:bg-sky-600 p-1 px-3">Edit</button>
+     </div>
+
+    <!-- ---------Others page-------------- -->
+    {#if $UserAllData?.navbarData}
+    {#each $UserAllData?.navbarData?.node1?.items as item}
+        {#if $UserAllData?.navbarData[item.id] }
+             <div class="rounded p-2 bg-white/10 font-semibold text-md flex items-center justify-between px-5">
+                <p class=" capitalize">
+                    {$UserAllData?.navbarData[item.id].name}
+                </p>
+                <a href={`/${$UserAllData?.websiteName}/${$UserAllData?.navbarData[item.id].name}`} class=" text-white/70 hover:text-white/90">
+                    /{$UserAllData?.websiteName}/{$UserAllData?.navbarData[item.id].name}
+                </a>
+                <button class="rounded bg-sky-500 hover:bg-sky-600 p-1 px-3">Edit</button>
+
+             </div>
+        {/if}
+        <!-- -------------sub-category page------------------ -->
+        {#if $UserAllData?.navbarData[item.id]?.items }
+             {#each $UserAllData?.navbarData[item.id].items as subItem}
+                {#if $UserAllData?.navbarData[subItem.id] }
+                <div class="rounded p-2 bg-white/10 font-semibold text-md flex items-center justify-between px-5">
+                    <p class=" capitalize">
+                        {$UserAllData?.navbarData[subItem.id].name}
+                    </p>
+                    <a href={`/${$UserAllData?.websiteName}/${$UserAllData?.navbarData[subItem.id].name}`} class=" text-white/70 hover:text-white/90">
+                        /{$UserAllData?.websiteName}/{$UserAllData?.navbarData[subItem.id].name}
+                    </a>
+                    <button class="rounded bg-sky-500 hover:bg-sky-600 p-1 px-3">Edit</button>
+
+                </div>
+                {/if}
+             {/each}
+        {/if}
+    {/each}
+    <!-- content here -->
+    {:else}
+         <!-- else content here -->
+         <div class="bg-red-100 p-5 text-red-500">No Data Found !!</div>
+    {/if}
+</div>
