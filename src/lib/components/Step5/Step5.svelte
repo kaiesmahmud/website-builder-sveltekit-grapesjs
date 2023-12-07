28044883abc0131c320e10bdfa711f6115466a6d
kaiesmahmud <kaiesmahmud@gmail.com>
2023-12-07 21:52:47 +0600

    added sub-components & updating logic

--- src/lib/components/Step5/Step5.svelte
+++ src/lib/components/Step5/Step5.svelte
@@ -1,10 +1,19 @@
 <script>
+	import Editor from './sub-components/Editor.svelte';
     import { ShowProcess } from "../../store/store";
+    import PagesList from './sub-components/PagesList.svelte';
 
-
+    let edit = false ;
+    $: toEdit = "home"
 </script>
 
 <div>
     <h1 class="font-semibold text-xl text-white p-3">STEP {$ShowProcess} of 6 | Page Customization</h1>
-
+    <div>
+        {#if edit}
+             <Editor/>
+        {:else}
+             <PagesList/>
+        {/if}
+    </div>
 </div>
