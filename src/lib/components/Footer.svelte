208c5e48ec75e87231fbdea4e9d63801ccc77d2f
kaiesmahmud <kaiesmahmud@gmail.com>
2023-12-09 02:16:19 +0600

    css

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,3 +1,3 @@
-<footer class="text-sm font-bold text-center text-transparent capitalize bg-clip-text bg-gradient-to-r from-cyan-400 to-pink-600 ">
+<footer class="text-sm font-bold text-center text-transparent capitalize bg-clip-text bg-gradient-to-r from-cyan-400 to-blue-600 ">
     Magicmind ltd
 </footer>
