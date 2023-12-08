a27c6b86faa95e2f2de3b4220f21477d3df615df
kaiesmahmud <kaiesmahmud@gmail.com>
2023-12-09 02:15:40 +0600

    updated

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,3 @@
 <footer class="text-sm font-bold text-center text-transparent capitalize bg-clip-text bg-gradient-to-r from-cyan-400 to-pink-600 ">
-    Developed By Kaies Mahmud Nehal <br>
-    2023 | November
+    Magicmind ltd
 </footer>
--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,3 +1,3 @@
-<h1 class="inline-block w-[100vw] py-5 text-3xl md:text-5xl font-bold  text-center text-transparent capitalize bg-clip-text bg-gradient-to-r from-sky-500 via-cyan-400 to-sky-600">
-    Custom-Website-Builder
+<h1 class="inline-block w-[100vw] py-5 text-xl md:text-3xl font-bold  text-center text-transparent capitalize bg-clip-text bg-gradient-to-r from-sky-500 via-cyan-400 to-sky-600">
+    Magic-Website-Creator
 </h1>
