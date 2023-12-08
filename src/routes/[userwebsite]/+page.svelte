3e49402be9bba12d98395c92f89914c88e132841
kaiesmahmud <kaiesmahmud@gmail.com>
2023-12-09 01:47:16 +0600

    html parsed in body

--- src/routes/[userwebsite]/+page.svelte
+++ src/routes/[userwebsite]/+page.svelte
@@ -1,7 +1,13 @@
 <script>
+    import { UserAllData } from '../../lib/store/store.js';
+	import { compareAndGiveData } from './../../lib/functions/compareAndGiveData.js';
+
     export let data
+
+    $: bodyData = compareAndGiveData(data.viewUserWebsiteName,data.viewUserWebsiteNameurl , $UserAllData.websiteName, $UserAllData.allPagesData)
 </script>
 
-<div>
-    {data.viewUserWebsiteName}
+<div class="bg-white w-full h-full min-h-screen text-black">
+    <p>{@html bodyData}</p>
+
 </div>
--- src/routes/[userwebsite]/[userwebsitepage]/+page.svelte
+++ src/routes/[userwebsite]/[userwebsitepage]/+page.svelte
@@ -1,7 +1,11 @@
 <script>
+    import { UserAllData } from '../../../lib/store/store.js';
+	import { compareAndGiveData } from './../../../lib/functions/compareAndGiveData.js';
+
     export let data
+    let getURL = "/"+data.viewUserWebsiteName+"/"+data.viewUserWebsitePage
+    $: bodyData = compareAndGiveData(data.viewUserWebsiteName, getURL , $UserAllData.websiteName, $UserAllData.allPagesData)
 </script>
-<div>
-    {data.viewUserWebsiteName}/
-    {data.viewUserWebsitePage}
+<div class="bg-white w-full h-full min-h-screen text-black">
+    <p>{@html bodyData}</p>
 </div>
